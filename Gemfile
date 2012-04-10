4c99588d0a0170023fb15761f3f02a22d49d7cd4
Subhi Beidas <subhibeidas@gmail.com>
2012-04-10 17:51:54 -0500

    a

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'pg'
+gem 'nifty-generators'
+gem "mocha", :group => :test
+gem 'geocoder'
---
+++ public/javascripts/application.js
@@ -0,0 +1,14 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+function findMe() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      document.getElementById('search').value = position.coords.latitude+' '+position.coords.longitude;
+     document.forms[0].submit();
+    }, function() {
+      alert('We couldn\'t find your position.');
+    });
+  } else {
+    alert('Your browser doesn\'t support geolocation.');
+  }
+}
