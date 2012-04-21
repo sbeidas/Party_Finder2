c88b6a325a6b8500cba5e4bcaa4e9aa53d90bfb4
Subhi Beidas <subhibeidas@gmail.com>
2012-04-20 23:17:16 -0500

    display parties not locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,11 @@
 class LocationsController < ApplicationController
   def index
   if params[:search].present?
+  	
     @locations = Location.near(params[:search], 50, :order => :distance)
   else
     @locations = Location.all
+    @parties=locations
   end
 end
 
