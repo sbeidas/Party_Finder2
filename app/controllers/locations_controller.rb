d5f6b93091024e63aa70133d79eccabec9e149a4
Subhi Beidas <subhibeidas@gmail.com>
2012-04-23 21:28:11 -0500

    added json

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,42 +1,66 @@
 class LocationsController < ApplicationController
   def index
   if params[:search].present?
-  	
     @locations = Location.near(params[:search], 5000000000000000000000000000000000000, :order => :distance)
-    @type=params[:search]
+    @locations=params[:search]
+     respond_to do |format|
+      format.html # index.html.erb
+      format.json  { render :json => @locations }
+    end
   else
     @locations = Location.all
+     respond_to do |format|
+      format.html # index.html.erb
+      format.json  { render :json => @locations }
+    end
 
   end
+  
 end
 
   def show
     @location = Location.find(params[:id])
+     respond_to do |format|
+      format.html # show.html.erb
+      format.json  { render :json => @location  }
+    end
   end
 
   def new
     @location = Location.new
+     respond_to do |format|
+      format.html # show.html.erb
+      format.json  { render :json => @location  }
+    end
   end
 
   def create
     @location = Location.new(params[:location])
     if @location.save
-      redirect_to @location, :notice => "Successfully created location."
+      format.html { redirect_to(@location, :notice => 'Party was successfully created.') }
+    format.json  { render :json => @location, :status => :created, :location => @location }
     else
-      render :action => 'new'
+     format.html { render :action => "new" }
+        format.json  { render :json => @location.errors, :status => :unprocessable_entity }
     end
   end
 
   def edit
     @location = Location.find(params[:id])
+     respond_to do |format|
+      format.html # show.html.erb
+      format.json  { render :json => @location  }
+    end
   end
 
   def update
     @location = Location.find(params[:id])
     if @location.update_attributes(params[:location])
-      redirect_to @location, :notice  => "Successfully updated location."
+     format.html { redirect_to(@location, :notice => 'Party was successfully updated.') }
+        format.json  { head :ok }
     else
-      render :action => 'edit'
+     format.html { render :action => "edit" }
+        format.json  { render :json => @location.errors, :status => :unprocessable_entity }
     end
   end
 
